f089860bf88d7336c18716ce7a107497ad94ee74
César <ivucesar@gmail.com>
2019-02-06 18:15:34 +0100

    He hecho el menu principal (codigo basico), he acabado de hacer la transferencia de personajes desde el array list, tambien he creado el boton salir.

--- Codigo/ProyectoIntegrado/app/src/main/java/educacion/trax/proyectointegrado/MainActivity.java
+++ Codigo/ProyectoIntegrado/app/src/main/java/educacion/trax/proyectointegrado/MainActivity.java
@@ -1,13 +1,49 @@
 package educacion.trax.proyectointegrado;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+import educacion.trax.proyectointegrado.clases.Personaje;
 
 public class MainActivity extends AppCompatActivity {
+    Button botonSiguiente, botonSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botonSiguiente = findViewById(R.id.buttonEmpezar);
+        botonSalir=findViewById(R.id.buttonSalirActivityMain);
+
+        botonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MenuPrincipal.class);
+                ArrayList<Personaje> personajesArray = new ArrayList<Personaje>();
+
+                Personaje pers1 = new Personaje("FelipeV","Warrior");
+
+                personajesArray.add(pers1);
+
+                i.putParcelableArrayListExtra("personajes",personajesArray);
+                startActivity(i);
+
+            }
+        });
+        botonSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
     }
 }
--- Codigo/ProyectoIntegrado/app/src/main/java/educacion/trax/proyectointegrado/MenuPrincipal.java
+++ Codigo/ProyectoIntegrado/app/src/main/java/educacion/trax/proyectointegrado/MenuPrincipal.java
@@ -1,23 +1,62 @@
 package educacion.trax.proyectointegrado;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.widget.Button;
 
+import java.io.Console;
 import java.util.ArrayList;
 
-public class MenuPrincipal extends AppCompatActivity {
+import educacion.trax.proyectointegrado.clases.Personaje;
+
+public class MenuPrincipal extends AppCompatActivity implements View.OnClickListener {
+    Button botonJugar,botonNuevaPartida,botonOpciones,botonSalir;
+    ArrayList personajes;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Bundle data = getIntent().getExtras();
-
-        ArrayList personajes = data.getParcelableArrayList("personajes");
-
-
-
-
-
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        Bundle data = getIntent().getExtras();
+
+        personajes = data.getParcelableArrayList("personajes");
+
+        Personaje pers1 = (Personaje) personajes.get(0);
+
+
+        botonJugar = (Button)findViewById(R.id.buttonJugar);
+        botonOpciones=(Button)findViewById(R.id.buttonOpciones);
+        botonSalir=(Button)findViewById(R.id.buttonSalirMenuPrincipal);
+        botonNuevaPartida=(Button)findViewById(R.id.buttonNuevaPartida);
+
+        botonJugar.setOnClickListener(this);
+        botonOpciones.setOnClickListener(this);
+        botonSalir.setOnClickListener(this);
+        botonNuevaPartida.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i=null;
+
+        if(v.getId()==botonJugar.getId()){
+            i = new Intent();
+        }else if(v.getId()==botonNuevaPartida.getId()){
+            i = new Intent();
+        }else if(v.getId()==botonOpciones.getId()){
+            i = new Intent();
+        }else if(v.getId()==botonSalir.getId()){
+            finish();
+        }
+
+        if(i!=null){
+            //i.putParcelableArrayListExtra();
+        }
     }
 }
